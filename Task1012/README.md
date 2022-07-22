ef56e7a4e69f512c89b8e4ab12789f8616e088ca
Hironori Ichimiya <hiron@hironytic.com>
2022-07-23 08:18:49 +0900

    Add Task1012 Q3. K 本の辺を選ぶ

---
+++ Task1012/README.md
@@ -0,0 +1,3 @@
+# Task1012
+
+https://algo-method.com/tasks/1012/
---
+++ Task1012/Sources/Task1012/validation.swift
@@ -0,0 +1,65 @@
+import Foundation
+#if DEVELOPING
+import TestData
+
+let data1 = TestData(name: "Data 1", text: """
+8 12 5
+0 1 11
+0 2 7
+0 4 8
+1 3 7
+1 7 6
+2 3 1
+2 4 10
+3 5 4
+3 6 2
+4 5 9
+5 6 3
+6 7 5
+""", expected: """
+17
+""")
+
+let data2 = TestData(name: "Data 2", text: """
+5 6 5
+3 0 67
+2 1 13
+0 2 50
+4 1 71
+3 4 24
+2 4 83
+""", expected: """
+-1
+""")
+
+let data3 = TestData(name: "Data 3", text: """
+""", expected: """
+""")
+
+let data4 = TestData(name: "Data 4", text: """
+""", expected: """
+""")
+
+let data5 = TestData(name: "Data 5", text: """
+""", expected: """
+""")
+
+let validationList = [
+    data1,
+    data2,
+//    data3,
+//    data4,
+//    data5,
+]
+
+// ----------------------------------------------------------
+func print(_ items: Any...) {
+    TestData.write(items)
+}
+
+func validate(_ main: (() -> String) -> Void) {
+    validationList.forEach { $0.execute(main) }
+}
+// ----------------------------------------------------------
+
+#endif
