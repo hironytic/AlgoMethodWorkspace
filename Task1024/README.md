a092c4d73a922191c69ccb448cefbc86b6cbc2c7
Hironori Ichimiya <hiron@hironytic.com>
2022-08-09 08:07:00 +0900

    Add Task1024 Q3. 3 つの数の和

---
+++ Task1024/README.md
@@ -0,0 +1,3 @@
+# Task1024
+
+https://algo-method.com/tasks/1024/
---
+++ Task1024/Sources/Task1024/main.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+func main(read: () -> String) {
+    let (N, M) = read().asList(ofInt).asTuple()
+    
+    var ans = 0
+    for x in 1 ... N {
+        for y in 1 ... N {
+            ans += min(max(M - (x + y), 0), N)
+        }
+    }
+    print(ans)
+}
+
+// ----------------------------------------------------------
+func ofInt<S: StringProtocol>(_ string: S) -> Int { Int(string)! }
+func ofDouble<S: StringProtocol>(_ string: S) -> Double { Double(string)! }
+extension StringProtocol {
+    func asInt() -> Int { ofInt(self) }
+    func asDouble() -> Double { ofDouble(self) }
+    func asList() -> [SubSequence] { split(separator: " ") }
+    func asList<T>(_ transform: (SubSequence) -> T) -> [T] { asList().map(transform) }
+}
+extension Array {
+    func asTuple() -> (Element, Element) { (self[0], self[1]) }
+    func asTuple() -> (Element, Element, Element) { (self[0], self[1], self[2]) }
+}
+// ----------------------------------------------------------
+
+#if DEVELOPING
+validate(main)
+#else
+main { readLine()! }
+#endif
