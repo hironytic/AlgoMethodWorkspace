b5cf0ff2d6490bf43924ee70db473e836a6fd0d3
Hironori Ichimiya <hiron@hironytic.com>
2022-08-14 00:02:00 +0900

    Add Task1027 Q3. 駅と駅の距離

---
+++ Task1027/README.md
@@ -0,0 +1,3 @@
+# Task1027
+
+https://algo-method.com/tasks/1027/
---
+++ Task1027/Sources/Task1027/main.swift
@@ -0,0 +1,36 @@
+import Foundation
+
+func main(read: () -> String) {
+    _ = read()
+    let D = read().asList(ofInt)
+    var S = [0]
+    for d in D {
+        S.append(S.last! + d)
+    }
+    let Q = read().asInt()
+    for _ in 0 ..< Q {
+        let (l, r) = read().asList(ofInt).asTuple()
+        print(S[r] - S[l])
+    }
+}
+
+// ----------------------------------------------------------
+func ofInt<S: StringProtocol>(_ string: S) -> Int { Int(string)! }
+func ofDouble<S: StringProtocol>(_ string: S) -> Double { Double(string)! }
+extension StringProtocol {
+    func asInt() -> Int { ofInt(self) }
+    func asDouble() -> Double { ofDouble(self) }
+    func asList() -> [SubSequence] { split(separator: " ") }
+    func asList<T>(_ transform: (SubSequence) -> T) -> [T] { asList().map(transform) }
+}
+extension Array {
+    func asTuple() -> (Element, Element) { (self[0], self[1]) }
+    func asTuple() -> (Element, Element, Element) { (self[0], self[1], self[2]) }
+}
+// ----------------------------------------------------------
+
+#if DEVELOPING
+validate(main)
+#else
+main { readLine()! }
+#endif
