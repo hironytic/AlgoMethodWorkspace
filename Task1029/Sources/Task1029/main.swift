bb08cee711c26036360a30b24d9d5d49df555c75
Hironori Ichimiya <hiron@hironytic.com>
2022-08-16 06:55:57 +0900

    Task1029 Q5. ひもの本数 （累積和で解いた）

--- Task1029/Sources/Task1029/main.swift
+++ Task1029/Sources/Task1029/main.swift
@@ -1,20 +1,23 @@
 import Foundation
 
 func main(read: () -> String) {
-    let N = read().asInt()
-    let L = read().asList(ofInt).sorted()
+    _ = read()
+    let L = read().asList(ofInt)
     
-    func count(lessThanOrEqualTo threshold: Int) -> Int {
-        return binarySearchThreshold(low: 0, high: N - 1) { L[$0] > threshold }
+    var C = [Int](repeating: 0, count: 100001)
+    for l in L {
+        C[l] += 1
     }
-    func count(lessThan threshold: Int) -> Int {
-        return binarySearchThreshold(low: 0, high: N - 1) { L[$0] >= threshold }
+    
+    var S = [Int]()
+    for c in C {
+        S.append((S.last ?? 0) + c)
     }
     
     let Q = read().asInt()
     for _ in 0 ..< Q {
         let (a, b) = read().asList(ofInt).asTuple()
-        print(count(lessThanOrEqualTo: b) - count(lessThan: a))
+        print(S[b] - S[a - 1])
     }
 }
 
@@ -32,50 +35,6 @@ extension Array {
     func asTuple() -> (Element, Element, Element) { (self[0], self[1], self[2]) }
 }
 // ----------------------------------------------------------
-// ----------------------------------------------------------
-/// 二分探索を行います。
-/// O(log n)
-/// - Parameters:
-///   - low: 下限インデックス
-///   - high: 上限インデックス
-///   - judge: 値を比較するクロージャー。
-///            引数に渡されたインデックスの値が期待するものより小さければ負の数、大きければ正の数、同じなら0を返します。
-/// - Throws: 引数に渡したクロージャーが投げたエラー
-/// - Returns: 結果のタプル。
-///            0番目の要素は見つかったかどうか（見つかればtrue）、1番目の要素は見つかったインデックスまたは見つからなかったときは期待する値を挿入するべきインデックス
-func binarySearch(low: Int, high: Int, judge: (Int) throws -> Int) rethrows -> (Bool, Int) {
-    var low = low
-    var high = high
-
-    while low <= high {
-        let mid = low + (high - low) / 2
-        let compResult = try judge(mid)
-        if compResult < 0 {
-            low = mid + 1
-        } else if compResult > 0 {
-            high = mid - 1
-        } else {
-            // 見つかった
-            return (true, mid)
-        }
-    }
-    
-    // 見つからなかった
-    return (false, low)
-}
-// ----------------------------------------------------------
-/// ある閾値以上になると条件を満たすようになる場合の、その閾値を二分探索で求めます。
-/// つまり、条件を満たす最小の値を返します。
-/// 上限値（high）でも条件を満たさなければ high + 1 が返ります。
-/// - Parameters:
-///   - low: 下限値
-///   - high: 上限値
-///   - judge: 引数に渡された値が条件を満たすかどうかを返すクロージャー
-/// - Throws: 引数に渡したクロージャーが投げたエラー
-/// - Returns: 条件を満たす最小の値を返します。
-func binarySearchThreshold(low: Int, high: Int, judge: (Int) throws -> Bool) rethrows -> Int {
-    return try binarySearch(low: low, high: high) { try judge($0) ? 1 : -1 }.1
-}
 
 #if DEVELOPING
 validate(main)
