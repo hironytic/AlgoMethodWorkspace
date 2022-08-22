b8b7f79910b5d4166390e44bf66cc01e3fbfc0a0
Hironori Ichimiya <hiron@hironytic.com>
2022-08-22 19:44:24 +0900

    Add Task1058 Q1. まとめてフラグを立てる

---
+++ Task1058/Sources/Task1058/main.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+func main(read: () -> String) {
+    let (A, M) = read().asList(ofInt).asTuple()
+    print(A | M)
+}
+
+// ----------------------------------------------------------
+func ofInt<S: StringProtocol>(_ string: S) -> Int { Int(string)! }
+func ofDouble<S: StringProtocol>(_ string: S) -> Double { Double(string)! }
+extension StringProtocol {
+    func asInt() -> Int { ofInt(self) }
+    func asDouble() -> Double { ofDouble(self) }
+    func asList() -> [SubSequence] { split(separator: " ") }
+    func asList<T>(_ transform: (SubSequence) -> T) -> [T] { asList().map(transform) }
+}
+extension Array {
+    func asTuple() -> (Element, Element) { (self[0], self[1]) }
+    func asTuple() -> (Element, Element, Element) { (self[0], self[1], self[2]) }
+}
+// ----------------------------------------------------------
+
+#if DEVELOPING
+validate(main)
+#else
+main { readLine()! }
+#endif
---
+++ Task1058/Sources/Task1058/validation.swift
@@ -0,0 +1,51 @@
+import Foundation
+#if DEVELOPING
+import TestData
+
+let data1 = TestData(name: "Data 1", text: """
+12 5
+""", expected: """
+13
+""")
+
+let data2 = TestData(name: "Data 2", text: """
+13 5
+""", expected: """
+13
+""")
+
+let data3 = TestData(name: "Data 3", text: """
+3 12
+""", expected: """
+15
+""")
+
+let data4 = TestData(name: "Data 4", text: """
+364418270 376388288
+""", expected: """
+402636510
+""")
+
+let data5 = TestData(name: "Data 5", text: """
+""", expected: """
+""")
+
+let validationList = [
+    data1,
+    data2,
+    data3,
+    data4,
+//    data5,
+]
+
+// ----------------------------------------------------------
+func print(_ items: Any...) {
+    TestData.write(items)
+}
+
+func validate(_ main: (() -> String) -> Void) {
+    validationList.forEach { $0.execute(main) }
+}
+// ----------------------------------------------------------
+
+#endif
