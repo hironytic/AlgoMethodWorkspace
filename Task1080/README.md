39e9c8dd535efcd39aa04eee1d5a9852adff201f
Hironori Ichimiya <hiron@hironytic.com>
2022-09-10 08:32:03 +0900

    Add Task1080 Q7. 共通部分と和集合

---
+++ Task1080/README.md
@@ -0,0 +1,3 @@
+# Task1080
+
+https://algo-method.com/tasks/1080/
---
+++ Task1080/Sources/Task1080/main.swift
@@ -0,0 +1,35 @@
+import Foundation
+
+func main(read: () -> String) {
+    let (_, X, Y) = read().asList(ofInt).asTuple()
+    print([(X & Y), (X | Y)].joinedString())
+}
+
+// ----------------------------------------------------------
+func ofInt<S: StringProtocol>(_ string: S) -> Int { Int(string)! }
+func ofDouble<S: StringProtocol>(_ string: S) -> Double { Double(string)! }
+extension StringProtocol {
+    func asInt() -> Int { ofInt(self) }
+    func asDouble() -> Double { ofDouble(self) }
+    func asList() -> [SubSequence] { split(separator: " ") }
+    func asList<T>(_ transform: (SubSequence) -> T) -> [T] { asList().map(transform) }
+}
+extension Array {
+    func asTuple() -> (Element, Element) { (self[0], self[1]) }
+    func asTuple() -> (Element, Element, Element) { (self[0], self[1], self[2]) }
+}
+extension Array where Element: LosslessStringConvertible {
+    /// 配列の内容を連結して1つの文字列にします。
+    /// - Parameter separator: セパレータ
+    /// - Returns: 連結した文字列
+    func joinedString(separator: String = " ") -> String {
+        return map { String($0) }.joined(separator: separator)
+    }
+}
+// ----------------------------------------------------------
+
+#if DEVELOPING
+validate(main)
+#else
+main { readLine()! }
+#endif
