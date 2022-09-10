64519c60c8afdff1e982ca83ec2a76e9058ca30e
Hironori Ichimiya <hiron@hironytic.com>
2022-09-11 07:37:32 +0900

    Add Task1082 Q1. 数列の部分和

---
+++ Task1082/README.md
@@ -0,0 +1,3 @@
+# Task1082
+
+https://algo-method.com/tasks/1082/
---
+++ Task1082/Sources/Task1082/main.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+func main(read: () -> String) {
+    let (_, X) = read().asList(ofInt).asTuple()
+    let A = read().asList(ofInt)
+    var ans = 0
+    for (i, a) in A.enumerated() {
+        if X & (1 << i) != 0 {
+            ans += a
+        }
+    }
+    print(ans)
+}
+
+// ----------------------------------------------------------
+func ofInt<S: StringProtocol>(_ string: S) -> Int { Int(string)! }
+func ofDouble<S: StringProtocol>(_ string: S) -> Double { Double(string)! }
+extension StringProtocol {
+    func asInt() -> Int { ofInt(self) }
+    func asDouble() -> Double { ofDouble(self) }
+    func asList() -> [SubSequence] { split(separator: " ") }
+    func asList<T>(_ transform: (SubSequence) -> T) -> [T] { asList().map(transform) }
+}
+extension Array {
+    func asTuple() -> (Element, Element) { (self[0], self[1]) }
+    func asTuple() -> (Element, Element, Element) { (self[0], self[1], self[2]) }
+}
+// ----------------------------------------------------------
+
+#if DEVELOPING
+validate(main)
+#else
+main { readLine()! }
+#endif
