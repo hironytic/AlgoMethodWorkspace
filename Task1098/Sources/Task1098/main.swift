f68deb9f7bbb191707e912bd271aeb859753a2f2
Hironori Ichimiya <hiron@hironytic.com>
2022-09-25 08:17:35 +0900

    Add Task1098 アイスの棒 (Easy)

---
+++ Task1098/Sources/Task1098/main.swift
@@ -0,0 +1,34 @@
+import Foundation
+
+func main(read: () -> String) {
+    let (N, M) = read().asList(ofInt).asTuple()
+    var ans = N
+    var bar = N    
+    while bar >= M {
+        let bonus = bar / M
+        ans += bonus
+        bar = bar % M + bonus
+    }
+    print(ans)
+}
+
+// ----------------------------------------------------------
+func ofInt<S: StringProtocol>(_ string: S) -> Int { Int(string)! }
+func ofDouble<S: StringProtocol>(_ string: S) -> Double { Double(string)! }
+extension StringProtocol {
+    func asInt() -> Int { ofInt(self) }
+    func asDouble() -> Double { ofDouble(self) }
+    func asList() -> [SubSequence] { split(separator: " ") }
+    func asList<T>(_ transform: (SubSequence) -> T) -> [T] { asList().map(transform) }
+}
+extension Array {
+    func asTuple() -> (Element, Element) { (self[0], self[1]) }
+    func asTuple() -> (Element, Element, Element) { (self[0], self[1], self[2]) }
+}
+// ----------------------------------------------------------
+
+#if DEVELOPING
+validate(main)
+#else
+main { readLine()! }
+#endif
