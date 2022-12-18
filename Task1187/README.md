3228f6dd4e42d65a16a24f0ebb93de63a061177a
Hironori Ichimiya <hiron@hironytic.com>
2022-12-18 10:24:28 +0900

    Add Task1187 Q3. 2 進法を 16 進法にするプログラム

---
+++ Task1187/README.md
@@ -0,0 +1,3 @@
+# Task1187
+
+https://algo-method.com/tasks/1187/
---
+++ Task1187/Sources/Task1187/main.swift
@@ -0,0 +1,43 @@
+import Foundation
+
+func main(read: () -> String) {
+    let N = read().asInt()
+    let S = read()
+    
+    let mapper: [String: String] = [
+        "0000": "0", "0001": "1", "0010": "2", "0011": "3",
+        "0100": "4", "0101": "5", "0110": "6", "0111": "7",
+        "1000": "8", "1001": "9", "1010": "A", "1011": "B",
+        "1100": "C", "1101": "D", "1110": "E", "1111": "F",
+    ]
+    
+    var ans = ""
+    for i in 0 ..< N / 4 {
+        let start = S.index(S.startIndex, offsetBy: i * 4)
+        let end = S.index(start, offsetBy: 3)
+        let s = String(S[start ... end])
+        ans += mapper[s]!
+    }
+    print(ans)    
+}
+
+// ----------------------------------------------------------
+func ofInt<S: StringProtocol>(_ string: S) -> Int { Int(string)! }
+func ofDouble<S: StringProtocol>(_ string: S) -> Double { Double(string)! }
+extension StringProtocol {
+    func asInt() -> Int { ofInt(self) }
+    func asDouble() -> Double { ofDouble(self) }
+    func asList() -> [SubSequence] { split(separator: " ") }
+    func asList<T>(_ transform: (SubSequence) -> T) -> [T] { asList().map(transform) }
+}
+extension Array {
+    func asTuple() -> (Element, Element) { (self[0], self[1]) }
+    func asTuple() -> (Element, Element, Element) { (self[0], self[1], self[2]) }
+}
+// ----------------------------------------------------------
+
+#if DEVELOPING
+validate(main)
+#else
+main { readLine()! }
+#endif
